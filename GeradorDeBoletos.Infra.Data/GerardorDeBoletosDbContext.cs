3e2cdf79f95fe3d6d5d9849ca7692e6f423b3a7d
johnboeira <johnboeira@gmail.com>
2025-02-15 15:44:15 -0300

    feat: inicia configuração do ef core com postgreSQL

---
+++ GeradorDeBoletos.Infra.Data/GerardorDeBoletosDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GeradorDeBoletos.Infra.Data;
+
+public class GerardorDeBoletosDbContext : DbContext
+{
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(GerardorDeBoletosDbContext).Assembly);
+
+        base.OnModelCreating(modelBuilder);
+    }
+}
--- GeradorDeBoletos.Web.API/Program.cs
+++ GeradorDeBoletos.Web.API/Program.cs
@@ -1,35 +1,44 @@
-namespace GeradorDeBoletos.Web.API
+using GeradorDeBoletos.Infra.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace GeradorDeBoletos.Web.API;
+
+public class Program
 {
-    public class Program
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+        builder.Services.AddDbContext<GerardorDeBoletosDbContext>(options =>
+            options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+        builder.Services.AddControllers();
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        var app = builder.Build();
+
+        using (var scope = app.Services.CreateScope())
         {
-            var builder = WebApplication.CreateBuilder(args);
-
-            // Add services to the container.
-
-            builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
-
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
-
-            app.UseHttpsRedirection();
-
-            app.UseAuthorization();
-
-
-            app.MapControllers();
-
-            app.Run();
+            var dbContext = scope.ServiceProvider.GetRequiredService<GerardorDeBoletosDbContext>();
+            dbContext.Database.Migrate();
         }
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        app.Run();
     }
 }
