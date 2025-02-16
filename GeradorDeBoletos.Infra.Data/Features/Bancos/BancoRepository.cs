78c157aaaa118545c6c0a861f801f7befa493cb1
johnboeira <johnboeira@gmail.com>
2025-02-16 17:33:08 -0300

    feat: Adiciona repositórios

---
+++ GeradorDeBoletos.Infra.Data/Features/Bancos/BancoRepository.cs
@@ -0,0 +1,37 @@
+﻿using GeradorDeBoletos.Domain.Features.Bancos;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualBasic;
+
+namespace GeradorDeBoletos.Infra.Data.Features.Bancos;
+
+public class BancoRepository
+{
+    private GerardorDeBoletosDbContext _dbContext;
+
+    public BancoRepository(GerardorDeBoletosDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public async Task CriaAsync(Banco banco)
+    {
+        await _dbContext.Bancos.AddAsync(banco);
+
+        await _dbContext.SaveChangesAsync();
+    }
+
+    public async Task<bool> ExisteAsync(int id)
+    {
+        return await _dbContext.Bancos.AnyAsync(b => b.Id == id);
+    }
+
+    public async Task<IEnumerable<Banco>> BuscaTodos()
+    {
+        return await _dbContext.Bancos.ToListAsync();
+    }
+
+    public async Task<Banco> Busca(int id)
+    {
+        return await _dbContext.Bancos.SingleOrDefaultAsync(b => b.Id == id);
+    }
+}
---
+++ GeradorDeBoletos.Infra.Data/Features/Boletos/BoletoRepository.cs
@@ -0,0 +1,35 @@
+﻿using GeradorDeBoletos.Domain.Features.Boletos;
+using Microsoft.EntityFrameworkCore;
+
+namespace GeradorDeBoletos.Infra.Data.Features.Boletos;
+
+public class BoletoRepository
+{
+    private GerardorDeBoletosDbContext _dbContext;
+
+    public BoletoRepository(GerardorDeBoletosDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public async Task CriaAsync(Boleto boleto)
+    {
+        await _dbContext.Boletos.AddAsync(boleto);
+
+        await _dbContext.SaveChangesAsync();
+    }
+
+    public async Task<IEnumerable<Boleto>> BuscaTodos()
+    {
+        return await _dbContext.Boletos
+            .Include(b => b.Banco)
+            .ToListAsync();
+    }
+
+    public async Task<Boleto> Busca(int id)
+    {
+        return await _dbContext.Boletos
+            .Include(b => b.Banco)
+            .SingleOrDefaultAsync(b => b.Id == id);
+    }
+}
